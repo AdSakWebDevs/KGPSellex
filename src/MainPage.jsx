f41f089c3c0d924e03a53b8eed077e996c51136d
Aditya Mukherjee <adityamukherjee.dgp@gmail.com>
2023-07-16 11:13:53 +0530

    staging version

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Section from "./components/Section";
+import Alert from "./components/Alert";
 
 function MainPage() {
   return (
     <>
       <Navbar/>
-      
+      <Alert/>
       <Section/>
     </>
   );
